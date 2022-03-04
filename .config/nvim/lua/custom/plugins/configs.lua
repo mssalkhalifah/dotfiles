6158c09776d8db2c6ee85fd3811ed13cf8abf75f
mssalkhalifah <mssalkhalifah@gmail.com>
2022-03-05 00:40:52 +0300

    added endure languaged for tree sitter

--- .config/nvim/lua/custom/plugins/configs.lua
+++ .config/nvim/lua/custom/plugins/configs.lua
@@ -1,7 +1,24 @@
 local M = {}
 
 M.treesitter = {
-   ensure_installed = {},
+   ensure_installed = {
+     "css",
+     "html",
+     "tsx",
+     "scss",
+     "regex",
+     "python",
+     "php",
+     "markdown",
+     "lua",
+     "json",
+     "javascript",
+     "godot_resource",
+     "c",
+     "c_sharp",
+     "cpp",
+     "bash",
+   },
 }
 
 M.nvimtree = {
