e755fa1714246ef7a93fe5618c311a0beb551b4a
Mohammad Alkhalifah <60398196+mssalkhalifah@users.noreply.github.com>
2024-02-08 12:40:25 +0300

    feat: Added java support

--- .config/nvchad-custom/configs/lspconfig.lua
+++ .config/nvchad-custom/configs/lspconfig.lua
@@ -2,55 +2,65 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require("lspconfig")
-local servers =
-	{ "tailwindcss", "tsserver", "html", "cssls", "jsonls", "terraformls", "omnisharp", "angularls" }
+local servers = {
+  "tailwindcss",
+  "tsserver",
+  "html",
+  "cssls",
+  "jsonls",
+  "terraformls",
+  "omnisharp",
+  "angularls",
+  "docker_compose_language_service",
+  "dockerls",
+}
 
 for _, lsp in ipairs(servers) do
-	local lsp_config = lspconfig[lsp]
+  local lsp_config = lspconfig[lsp]
 
-	if lsp == "omnisharp" then
-		local pid = vim.fn.getpid()
-		local home = os.getenv("HOME")
-		local omnisharp_bin = home .. "/.local/share/nvim/mason/bin/omnisharp"
-		lsp_config.setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-			cmd = {
-				omnisharp_bin,
-				"--languageserver",
-				"--hostPID",
-				tostring(pid),
-			},
-		})
-	elseif lsp == "angularls" then
-		local project_lib_path = vim.fn.expand("$HOME")
-			.. "/.local/share/nvim/mason/packages/angular-language-server/node_modules/@angular/language-server/node_modules"
-		local project_ts_path = vim.fn.getcwd() .. "/node_modules"
-		local cmd =
-			{ "ngserver", "--stdio", "--tsProbeLocations", project_ts_path, "--ngProbeLocations", project_lib_path }
+  if lsp == "omnisharp" then
+    local pid = vim.fn.getpid()
+    local home = os.getenv("HOME")
+    local omnisharp_bin = home .. "/.local/share/nvim/mason/bin/omnisharp"
+    lsp_config.setup({
+      on_attach = on_attach,
+      capabilities = capabilities,
+      cmd = {
+        omnisharp_bin,
+        "--languageserver",
+        "--hostPID",
+        tostring(pid),
+      },
+    })
+  elseif lsp == "angularls" then
+    local project_lib_path = vim.fn.expand("$HOME")
+        .. "/.local/share/nvim/mason/packages/angular-language-server/node_modules/@angular/language-server/node_modules"
+    local project_ts_path = vim.fn.getcwd() .. "/node_modules"
+    local cmd =
+    { "ngserver", "--stdio", "--tsProbeLocations", project_ts_path, "--ngProbeLocations", project_lib_path }
 
-		lsp_config.setup({
-			cmd = cmd,
-			on_attach = on_attach,
-			capabilities = capabilities,
-			on_new_config = function(new_config, new_root_dir)
-				new_config.cmd = cmd
-			end,
-		})
-	elseif lsp == "tsserver" then
-		lsp_config.setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-			init_options = {
-				preferences = {
-					disableSuggestions = true,
-				},
-			},
-		})
-	else
-		lsp_config.setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-		})
-	end
+    lsp_config.setup({
+      cmd = cmd,
+      on_attach = on_attach,
+      capabilities = capabilities,
+      on_new_config = function(new_config, new_root_dir)
+        new_config.cmd = cmd
+      end,
+    })
+  elseif lsp == "tsserver" then
+    lsp_config.setup({
+      on_attach = on_attach,
+      capabilities = capabilities,
+      init_options = {
+        preferences = {
+          disableSuggestions = true,
+        },
+      },
+    })
+  else
+    lsp_config.setup({
+      on_attach = on_attach,
+      capabilities = capabilities,
+    })
+  end
 end
--- .config/nvchad-custom/plugins.lua
+++ .config/nvchad-custom/plugins.lua
@@ -1,5 +1,10 @@
 local plugins = {
   {
+    "mfussenegger/nvim-jdtls",
+    dependencies = "neovim/nvim-lspconfig",
+    event = "BufRead",
+  },
+  {
     "rcarriga/nvim-dap-ui",
     event = "VeryLazy",
     dependencies = "mfussenegger/nvim-dap",
@@ -16,92 +21,94 @@ local plugins = {
       dap.listeners.before.event_exited["dapui_config"] = function()
         dapui.close()
       end
-    end
+    end,
   },
   {
     "mfussenegger/nvim-dap",
     config = function()
-      require "custom.configs.dap"
+      require("custom.configs.dap")
       require("core.utils").load_mappings("dap")
-    end
-  },
-	{
-		"alexghergh/nvim-tmux-navigation",
-		config = function()
-			require("nvim-tmux-navigation").setup({})
-		end,
-		lazy = false,
-	},
-	{
-		"nvim-lua/plenary.nvim",
-	},
-	{
-    "jose-elias-alvarez/null-ls.nvim",
-    event = "VeryLazy",
-    opts = function()
-      return require "custom.configs.null-ls"
     end,
   },
   {
-		"neovim/nvim-lspconfig",
-		config = function()
-			require("plugins.configs.lspconfig")
-			require("custom.configs.lspconfig")
-		end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = {
-			ensure_installed = {
+    "alexghergh/nvim-tmux-navigation",
+    config = function()
+      require("nvim-tmux-navigation").setup({})
+    end,
+    lazy = false,
+  },
+  {
+    "nvim-lua/plenary.nvim",
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require("custom.configs.null-ls")
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("plugins.configs.lspconfig")
+      require("custom.configs.lspconfig")
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
         "angular",
-				"lua",
-				"c_sharp",
-				"css",
-				"bash",
-				"git_config",
-				"git_rebase",
-				"gitattributes",
-				"gitcommit",
-				"gitignore",
-				"html",
-				"javascript",
-				"json",
-				"markdown",
-				"python",
-				"tsx",
-				"typescript",
-				"dockerfile",
-				"terraform",
-			},
-		},
-	},
-	{
-		"williamboman/mason.nvim",
-		opts = {
-			ensure_installed = {
-				"css-lsp",
-				"html-lsp",
-				"json-lsp",
-				"eslint-lsp",
+        "lua",
+        "c_sharp",
+        "css",
+        "bash",
+        "git_config",
+        "git_rebase",
+        "gitattributes",
+        "gitcommit",
+        "gitignore",
+        "html",
+        "javascript",
+        "json",
+        "markdown",
+        "python",
+        "tsx",
+        "typescript",
+        "dockerfile",
+        "terraform",
+        "java",
+      },
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "jdtls",
+        "css-lsp",
+        "html-lsp",
+        "json-lsp",
+        "eslint-lsp",
         "js-debug-adapter",
-				"lua-language-server",
-				"sqlls",
-				"sqls",
-				"tailwindcss-language-server",
-				"typescript-language-server",
-				"prettier",
-				"stylua",
-				"shellcheck",
-				"docker-compose-language-service",
-				"dockerfile-language-server",
-				"csharp-language-server",
-				"angular-language-server",
-			},
-		},
-	},
-	{
-		"max397574/better-escape.nvim",
-	},
+        "lua-language-server",
+        "sqlls",
+        "sqls",
+        "tailwindcss-language-server",
+        "typescript-language-server",
+        "prettier",
+        "stylua",
+        "shellcheck",
+        "docker-compose-language-service",
+        "dockerfile-language-server",
+        "csharp-language-server",
+        "angular-language-server",
+      },
+    },
+  },
+  {
+    "max397574/better-escape.nvim",
+  },
 }
 
 return plugins
