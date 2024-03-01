306a9a593c5623e28bd90533e1167b0734a89797
mssalkhalifah <mssalkhalifah@gmail.com>
2024-03-01 19:43:17 +0300

    feat: added cargo

--- .setup.sh
+++ .setup.sh
@@ -11,21 +11,21 @@ command_exists() {
 
 if command_exists brew; then
   brew update && brew upgrade
-  brew install node rust golang zsh ripgrep tmux kitty
+  brew install node rust golang zsh ripgrep tmux kitty cargo
 fi
 
 if command_exists dnf; then
   sudo dnf update && sudo dnf upgrade
-  sudo dnf install -y nodejs rust golang zsh ripgrep tmux kitty
+  sudo dnf install nodejs rust golang zsh ripgrep tmux kitty cargo
 fi
 
 if command_exists apt; then
   sudo apt update && sudo apt upgrade
-  sudo apt install -y nodejs rustc golang zsh ripgrep tmux kitty
+  sudo apt install nodejs rustc golang zsh ripgrep tmux kitty cargo
 fi
 
-if ! [ -d $HOME/.config/nvim/lua/custom ]; then
-  ln -s $HOME/.config/nvchad-custom $HOME/.config/nvim/lua/custom
+if ! [ -d "$HOME/.config/nvim/lua/custom" ]; then
+  ln -s "$HOME/.config/nvchad-custom" "$HOME/.config/nvim/lua/custom"
 fi
 
 chsh -s "$(which zsh)"
