39c8ac3e0df80c025b23d90c3015f7919287b740
Mohammad Alkhalifah <mssalkhalifah@gmail.com>
2025-06-01 08:47:16 +0300

    Added dotnet sdks to packages and neovim setup script

---
+++ scripts/setup-neovim.sh
@@ -0,0 +1,8 @@
+!#/bin/sh
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+
+rm nvim-linux-x86_64.tar.gz
