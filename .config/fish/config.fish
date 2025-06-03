551a3e655104234c9db429511b19645b5798daff
Mohammad Alkhalifah <mssalkhalifah@gmail.com>
2025-06-03 10:28:35 +0300

    Updated

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -x KUBECONFIG $HOME/.kube/configs/*
     set -x PATH $PATH:/opt/nvim-linux-x86_64/bin
+
+    # aliases
+    alias ll="ls -la"
 end
