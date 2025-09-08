50b17119c4cb2e176bb739961c4cc08112d627fa
moalkhalifah <moalkhalifah@elm.sa>
2025-09-08 22:17:53 +0300

    added htop and others

---
+++ scripts/setup-dotnet-tools.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+dotnet tool install --global dotnet-ef
+dotnet tool install --global Volo.Abp.Studio.Cli
--- scripts/setup-neovim.sh
+++ scripts/setup-neovim.sh
@@ -2,7 +2,9 @@
 
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 
-sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+mkdir -p $HOME/.local/bin
+tar -xzf nvim-linux-x86_64.tar.gz
+cp nvim-linux-x86_64/bin/nvim $HOME/.local/bin/nvim
 
+rm -rf nvim-linux-x86_64
 rm nvim-linux-x86_64.tar.gz
