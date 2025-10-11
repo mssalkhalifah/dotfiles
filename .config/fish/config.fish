927ce9b19f1d17c1250a39702d92cfedc0dd26a2
Mohammad Alkhalifah <mssalkhalifah@gmail.com>
2025-10-11 10:24:07 +0300

    Added bitwarden SSH agent

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -x KUBECONFIG $HOME/.kube/config/*
     set -x PATH $PATH:/opt/nvim-linux-x86_64/bin
+    set -x PATH $PATH:/home/mssalkhalifah/.dotnet/tools
+    set -x SSH_AUTH_SOCK $HOME/.bitwarden-ssh-agent.sock
+
+    nvm use lts
 
     # aliases
     alias ll="ls -la"
