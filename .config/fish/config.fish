b9701a5c38dfde7e01b08fcf45f24c1f36244cac
Mohammad Alkhalifah <mssalkhalifah@gmail.com>
2025-11-06 15:00:21 +0300

    Added .ideavimrc

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,13 @@
+set -x SSH_AUTH_SOCK $HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    set -x KUBECONFIG $HOME/.kube/config/*
-    set -x PATH $PATH:/opt/nvim-linux-x86_64/bin
-    set -x PATH $PATH:/home/mssalkhalifah/.dotnet/tools
-    set -x SSH_AUTH_SOCK $HOME/.bitwarden-ssh-agent.sock
+    set -x KUBECONFIG $HOME/.kube/*.conf
+    # set -x PATH $PATH:/opt/nvim-linux-x86_64/bin
+    # set -x PATH $PATH:/home/mssalkhalifah/.dotnet/tools
+    # set -x SSH_AUTH_SOCK $HOME/.bitwarden-ssh-agent.sock
 
-    nvm use lts
+    # nvm use lts
 
     # aliases
     alias ll="ls -la"
