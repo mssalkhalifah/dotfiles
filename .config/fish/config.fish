718636dda7b6ebe21840569dbb18d367fa1c33a1
moalkhalifah <moalkhalifah@elm.sa>
2025-12-01 16:12:24 +0300

    Updated config for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ set -x SSH_AUTH_SOCK $HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-ag
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    set -x KUBECONFIG $HOME/.kube/*.conf
+    set -x KUBECONFIG $HOME/.kube/config
     # set -x PATH $PATH:/opt/nvim-linux-x86_64/bin
     # set -x PATH $PATH:/home/mssalkhalifah/.dotnet/tools
     # set -x SSH_AUTH_SOCK $HOME/.bitwarden-ssh-agent.sock
